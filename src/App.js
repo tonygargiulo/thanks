271faa8182f4e54f201ea4f8dda8ea15793f8963
tonygargiulo <tony@gargiulo.com>
2019-03-27 17:14:50 -0700

    starting from scratch, deleted everything in src, got hello world to render

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
-import React, { Component } from 'react';
-import Quote from './components/Quote';
-import axios from  'axios';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
+import  React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends Component {
-constructor(){
-  super();
-  this.state = {
-    ListItems: [],
+
+class App extends React.Component {
+  render() {
+    return <div>"hello world"</div>
   }
-};
-
-componentDidMount() {
-  let mappedListItems = [];
-  let updateList = function(res) {
-      if (res.status !== 200) return;
-      // make array of list components
-      mappedListItems =  res.data.map(item => {
-        if (item.id === 0){
-          return <div key={item.id}><ListItem button><ListItemText primary={item.body}/></ListItem></div>;
-        } else {
-          return <div key={item.id}><Divider /><ListItem button><ListItemText primary={item.body}/></ListItem></div>;
-        }
-      });
-      this.setState({
-        ListItems: mappedListItems,
-      });
-  }.bind(this);
-
-  axios.get('https://jsonplaceholder.typicode.com/posts').then(updateList);
-};
-
-render() {
-  return (
-    <div className="App">
-      <Quote />
-      <List>
-        {this.state.ListItems}
-      </List>
-    </div>
-  );
-};
-
-};
+}
 
 export default App;
